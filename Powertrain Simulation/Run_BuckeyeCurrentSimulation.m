37b80877e4787822d8bc5cb73904e688359f3119
ZSalyer <41242345+ZSalyer@users.noreply.github.com>
2018-10-02 19:42:18 -0400

    Added some stuff

--- Powertrain Simulation/Run_BuckeyeCurrentSimulation.m
+++ Powertrain Simulation/Run_BuckeyeCurrentSimulation.m
@@ -18,9 +18,56 @@ sim('BuckeyeCurrent_Simulation');
 %% Process Results and Plotting --- Chris add your plots here
 
 %Vehicle Speed vs Distance and Target Speed Versus Distance
+f = figure('color',[1 1 1]);
+title('Velocity Comparison');
+h(1) = plot(Veh_Distance,Veh_Velocity);
+%Should also plot the entire velocity profile to see how far we made it,
+%need to figure out how to export this from simulink or load it
+%automatically here
+set(h(1),'linewidth',2);
+ylabel('Velocity (m/s');
+xlabel('Distance (m)');
+legend('Simulated Velocity');
+
 
 %Pack Voltage, Current, Power, Energy, SOC -- Maybe make these on the same
 %figure but different plots, if you google 'matlab subplot' that'd help
+f = figure('color',[1 1 1]);
+title('Pack Performance');
+subplot(311)
+h(1) = plot(Batt_Voltage);
+hold on;
+grid on;
+h(2) = plot(Batt_Current);
+ylabel('Voltage/Current (V/A)');
+yyaxis right
+h(3) = plot(Batt_Power);
+set(h(1),'linewidth',2);
+set(h(2),'linewidth',2);
+set(h(3),'linewidth',2);
+legend('Pack Voltage','Pack Current','Pack Power','Location','northwest')
+ylabel('Power (kW)')
+xlabel('Time (s)')
+
+subplot(312)
+h(1) = plot(Batt_SOC);
+hold on;
+grid on;
+ylabel('SOC');
+yyaxis right
+h(2) = plot(Batt_Energy);
+ylabel('Energy Used kWh');
+set(h(1),'linewidth',2);
+set(h(2),'linewidth',2);
+legend('Pack SOC','Energy Useage','Location','northwest')
+xlabel('Time (s)')
+
+subplot(313)
+h(1) = plot(Batt_TerminalVoltage);
+set(h(1),'linewidth',2);
+ylabel('Terminal Voltage (V)');
+xlabel('Time (s)');
+legend('Terminal Voltage');
 
 %Motor torque, speed
 
