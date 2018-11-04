6847c14de77b956dbbfeb263631ead161452eeb6
ZSalyer <41242345+ZSalyer@users.noreply.github.com>
2018-11-04 12:43:06 -0500

    Changed gearing to ideal for Parker motor and added current plot

--- Powertrain Simulation/Run_BuckeyeCurrentSimulation.m
+++ Powertrain Simulation/Run_BuckeyeCurrentSimulation.m
@@ -18,7 +18,7 @@ sim('BuckeyeCurrent_Simulation');
 %% Process Results and Plotting --- Chris add your plots here
 
 %Vehicle Speed vs Distance and Target Speed Versus Distance
-f = figure('color',[1 1 1]);
+f1 = figure('color',[1 1 1]);
 title('Velocity Comparison');
 h(1) = plot(Veh_Distance,Veh_Velocity);
 %Should also plot the entire velocity profile to see how far we made it,
@@ -32,7 +32,7 @@ legend('Simulated Velocity');
 
 %Pack Voltage, Current, Power, Energy, SOC -- Maybe make these on the same
 %figure but different plots, if you google 'matlab subplot' that'd help
-f = figure('color',[1 1 1]);
+f2 = figure('color',[1 1 1]);
 title('Pack Performance');
 ax(1) = subplot(311);
 h(1) = plot(Batt_Voltage);
@@ -73,3 +73,9 @@ linkaxes(ax,'x')
 %Motor torque, speed
 
 %Anything else you think would be interesting
+
+figure('color', [1 1 1])
+title('IOM Current Profile - Unfiltered')
+plot(Batt_Current);
+ylabel('Batt Current [A]');
+xlabel('Time [s]');
